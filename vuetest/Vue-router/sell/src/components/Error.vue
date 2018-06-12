538dfed2d13ac4499b3a05d1fec049165b80ea77
你瞅啥！！！ <971039649@qq.com>
2018-06-12 10:20:19 +0800

    404

---
+++ vuetest/Vue-router/sell/src/components/Error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+  data(){
+    return {
+      msg: '404 undefined',
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
